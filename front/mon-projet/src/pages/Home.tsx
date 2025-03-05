7f60e6a32fbc56f72fe25dcbc411e60b03ac03bf
Klafoutis <kl4ra1234@gmail.com>
2025-03-05 10:31:36 +0100

    J'ai rien compris au style de la home page mais oklm

--- front/mon-projet/src/pages/Home.tsx
+++ front/mon-projet/src/pages/Home.tsx
@@ -1,15 +1,16 @@
 // src/pages/Home.tsx
 import React from 'react';
 import Citation, {CitationType} from "../components/Citation";
+import './Home.css';
 
 const Home: React.FC = () => {
     return (
-        <div>
-            <h1>Accueil</h1>
+        <div className="quote-of-the-day">
+            <h1>Citation du jour</h1>
             <Citation type={CitationType.Bordered} />
             <h2>Autres citations :</h2>
             <Citation />
-            <Citation color="RED" />
+            <Citation />
         </div>
     );
 };
